74635786cc579748bd32d22e70de9dfd4c2d0f65
Yasuhisa Yoshida <syou6162@gmail.com>
2016-08-01 20:59:16 +0900

    initial commit

---
+++ github.ts
@@ -0,0 +1,28 @@
+module Github {
+    var x = 0;
+    var repositories = [];
+    export function insertRepositoryName() {
+	if (x >= repositories.length) return;
+	var item = repositories[x];
+	var repos = document.getElementById('repositories');
+	var tmp = document.createElement('li');
+	tmp.innerHTML = item.name;
+	repos.appendChild(tmp);
+	x += 1;
+    }
+    function getRepositories() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', encodeURI('https://api.github.com/users/syou6162/repos'), false);
+	xhr.onload = function() {
+	    if (xhr.status === 200) {
+		repositories =  JSON.parse(xhr.responseText).slice(0, 3);
+	    } else {
+		alert('Request failed.  Returned status of ' + xhr.status);
+	    }
+	};
+	xhr.send();
+    }
+    window.onload = () => {
+	getRepositories();
+    }
+}
